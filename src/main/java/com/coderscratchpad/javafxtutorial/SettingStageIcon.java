eb36e954175e2b5edd2f5198e323d9f8ab3a21a9
edwardalgorist <43573459+edwardalgorist@users.noreply.github.com>
2023-10-19 15:51:11 +0200

    Added the SettingStageIcon class.

---
+++ src/main/java/com/coderscratchpad/javafxtutorial/SettingStageIcon.java
@@ -0,0 +1,37 @@
+package com.coderscratchpad.javafxtutorial;
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+/**
+ * Code of the tutorial described on
+ * <a href="https://coderscratchpad.com/adding-an-icon-to-your-javafx-stage/">coderscratchpad.com</a>
+ */
+public class SettingStageIcon extends Application {
+
+    private static final double WIDTH  = 640;
+    private static final double HEIGHT = 480;
+
+    @Override
+    public void start(Stage stage) {
+
+        BorderPane layoutManager = new BorderPane();
+        Scene scene = new Scene(layoutManager, WIDTH, HEIGHT);
+
+        Image icon = new Image("duck.png");
+
+        /* set stage icon */
+        stage.getIcons().add(icon);
+
+        stage.setTitle("Adding an Icon to Your JavaFX Stage");
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.show();
+
+    }
+
+}
