4f1a632d0776c04cf4562cb178426647bb86fc32
edwardalgorist <43573459+edwardalgorist@users.noreply.github.com>
2023-10-19 17:16:53 +0200

    Added SettingBackgroundImage and AccordionExampleApp classes.

---
+++ src/main/java/com/coderscratchpad/javafxtutorial/AccordionExampleApp.java
@@ -0,0 +1,73 @@
+package com.coderscratchpad.javafxtutorial;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+/**
+ * Code of the tutorial described on
+ * <a href="https://coderscratchpad.com/getting-started-with-accordions-in-javafx-for-beginners/">coderscratchpad.com</a>
+ */
+public class AccordionExampleApp extends Application {
+
+    private Scene scene;
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+        buildUI();
+    }
+
+    private void buildUI() {
+
+        // Create the main content pane using a VBox with vertical spacing of 10 and padding of 15
+        VBox mainContent = new VBox(10);
+        mainContent.setPadding(new Insets(15));
+
+        // Create the header label
+        Label header = new Label("FAQs");
+        header.setStyle("-fx-font-size: 20px; -fx-font-weight: bold");
+
+        // Create the Accordion container
+        Accordion accordion = new Accordion();
+
+        // Create nodes for the content sections
+        Label content1 = new Label("Content 1");
+        Label content2 = new Label("Content 2");
+        Label content3 = new Label("Content 3");
+        Label content4 = new Label("Content 4");
+
+        // Create titled panes for the questions
+        TitledPane question1 = new TitledPane("Is JavaFX still used today?", content1);
+        TitledPane question2 = new TitledPane("What is JavaFX used for?", content2);
+        TitledPane question3 = new TitledPane("Should I use JavaFX or Swing?", content3);
+        TitledPane question4 = new TitledPane("Is JavaFX available for Android?", content4);
+
+        // Add the titled panes to the accordion
+        accordion.getPanes().addAll(question1, question2, question3, question4);
+
+        // Add the header and accordion to the main content pane
+        mainContent.getChildren().addAll(header, accordion);
+
+        // Create the layout manager using BorderPane
+        BorderPane layoutManager = new BorderPane(mainContent);
+
+        // Create the scene with specified dimensions
+        this.scene = new Scene(layoutManager, 640, 480);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        // Set the scene for the stage
+        stage.setScene(this.scene);
+
+        stage.setTitle("Getting Started with Accordions in JavaFX for Beginners");
+
+        stage.centerOnScreen();
+        stage.show();
+    }
+}
---
+++ src/main/java/com/coderscratchpad/javafxtutorial/SettingBackgroundImage.java
@@ -0,0 +1,130 @@
+package com.coderscratchpad.javafxtutorial;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+/**
+ * Code of the tutorial described on
+ * <a href="https://coderscratchpad.com/creating-a-stylish-user-registration-form-with-background-image-in-javafx/">coderscratchpad.com</a>
+ */
+public class SettingBackgroundImage extends Application {
+
+    private Scene scene;
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+        buildUI();
+    }
+
+    private void buildUI() {
+
+        // create the main content pane
+        VBox mainContent = new VBox(15);
+
+        // set main content pane background image
+        setRegionBackgroundImage(mainContent, "form-background.jpg");
+
+        mainContent.setAlignment(Pos.CENTER);
+
+        // create input fields
+        VTextField name = new VTextField("Name");
+        name.setMaxWidth(300.0);
+
+        VTextField email = new VTextField("Email");
+        email.setMaxWidth(300.0);
+
+        VTextField password = new VTextField("Password", true);
+        password.setMaxWidth(300.0);
+
+        VTextField confirmPassword = new VTextField("Confirm Password", true);
+        confirmPassword.setMaxWidth(300.0);
+
+        // create checkbox for terms and conditions
+        CheckBox agreeToTerms = new CheckBox("I agree to the terms and conditions.");
+
+        // create register button
+        Button register = new Button("Register");
+
+        // create container for checkbox and register button
+        VBox buttonContainer = new VBox(15, agreeToTerms, register);
+        buttonContainer.setMaxWidth(300.0);
+
+        // add all elements to the main content pane
+        mainContent.getChildren().addAll(
+                name,
+                email,
+                password,
+                confirmPassword,
+                buttonContainer
+        );
+
+        // create the layout manager
+        BorderPane layoutManager = new BorderPane(mainContent);
+
+        // create the scene with specified dimensions
+        this.scene = new Scene(layoutManager, 640.0,480.0);
+
+    }
+
+    private void setRegionBackgroundImage(Region region, String imageUrl) {
+
+        // load the background image
+        Image image = new Image(imageUrl);
+
+        // create the background image with specified properties
+        BackgroundImage backgroundImage = new BackgroundImage(
+                image,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT
+        );
+
+        // apply the background image to the region
+        Background background = new Background(backgroundImage);
+        region.setBackground(background);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        stage.setScene(this.scene);
+        stage.setTitle("Creating a Stylish User Registration Form with Background Image in JavaFX");
+        stage.centerOnScreen();
+
+        stage.show();
+
+    }
+
+    static class VTextField extends VBox {
+
+        private final TextField textField;
+
+        public VTextField(String label) {
+            this(label, false);
+        }
+
+        public VTextField(String label, boolean obscure) {
+
+            if(obscure)
+                this.textField = new PasswordField();
+            else
+                this.textField = new TextField();
+
+            this.getChildren().addAll(new Label(label), this.textField);
+
+        }
+
+        public String getText() {
+            return this.textField.getText();
+        }
+
+    }
+
+}
